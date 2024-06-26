3199866b337c86ae1676c9a7e79dbf13b4e179cd
ThePope05 <swobben05@gmail.com>
2024-06-26 14:00:32 +0200

    Basis functionaliteit voor User story 7 toegevoegd

--- app/Models/Leverancier.php
+++ app/Models/Leverancier.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Leverancier extends Model
 {
@@ -29,5 +30,26 @@ class Leverancier extends Model
     {
         return $this->belongsToMany(Contact::class, 'contact_per_leveranciers', 'leverancier_id', 'contact_id')
             ->withTimestamps();
+    }
+
+    public function getLeveranciers($zoeknaam)
+    {
+        $leveranciers = Leverancier::has('contacts')
+            ->with('contacts')
+            ->where('leverancier_type', 'like', '%' . $zoeknaam . '%')
+            ->orderBy('naam')
+            ->get();
+
+        return $leveranciers;
+    }
+
+    public function getLeverancierTypes()
+    {
+        $leverancierTypes = Leverancier::select('leverancier_type')
+            ->distinct()
+            ->orderBy('leverancier_type')
+            ->get();
+
+        return $leverancierTypes;
     }
 }
