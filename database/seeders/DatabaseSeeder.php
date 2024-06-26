034fc9ea799b91e3718f9b82f0a898aa7156bfae
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-06-26 13:21:58 +0200

    run de andere seeder met db:seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GezinSeeder::class,
             PersoonSeeder::class,
-            // AllergieSeeder::class,
-            // AllergiePerPersoonSeeder::class, 
+            AllergieSeeder::class,
+            AllergiePerPersoonSeeder::class,
         ]);
     }
 }
