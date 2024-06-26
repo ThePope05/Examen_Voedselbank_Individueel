d3e8465ea6f7d8ec5623bd0d17f72c8922db1230
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2024-06-26 14:32:46 +0200

    Route voor voorraad overzicht en filter

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VoorraadController;
 
+// Homepage
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Voorraad overzicht
+Route::get('/voorraad', [VoorraadController::class, 'index'])->name('voorraad.index');
+
+// Filter voorraad op categorie
+Route::get('/voorraad/filter', [VoorraadController::class, 'filterByCategorie'])->name('voorraad.filterByCategorie');
