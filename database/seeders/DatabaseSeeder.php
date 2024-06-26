77f8c8d151527d28c1a01e7acb2b65699fba9f5f
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2024-06-26 12:05:34 +0200

    DatabaseSeeder aangepast voor de --seed command

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            CategorieSeeder::class,
+            MagazijnSeeder::class,
+            ProductSeeder::class,
+            ProductPerMagazijnSeeder::class,
         ]);
     }
 }
